c9e31ca0044122a2aa697419ab5d37a66009b7b5
Anton Agaronov <144243055+AntonAgaronov@users.noreply.github.com>
2025-06-23 22:12:34 +0400

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
+}
